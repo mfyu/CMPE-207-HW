42bc09ba4154fbd62123e672d572503d39e2c8ca
mfyu <myu121@gmail.com>
2020-04-01 12:45:26 -0700

    hw2

---
+++ hw2/client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 9000
+BUFFER_SIZE = 1024
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+#s.send(MESSAGE.encode())
+
+
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print("received data: \n", data.decode("utf-8"))
---
+++ hw2/server.py
@@ -0,0 +1,30 @@
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 9000
+BUFFER_SIZE = 1024
+M1 = "      ___            ____                 ___   __        ___\n"
+M2 = "|   | |    |    |    |    |    \\        / |   | |  | |    |   \\\n"
+M3 = "|___| |___ |    |    |    |     \\  /\\  /  |   | |_/  |    |    |\n"
+M4 = "|   | |___ |___ |___ |____|      \\/  \\/   |___| | \\  |___ |___/\n"
+
+MESSAGE = M1+M2+M3+M4
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+
+while 1:
+	s.listen(1)
+
+	conn, addr = s.accept()
+	print('Connection address:', addr)
+	conn.send(MESSAGE.encode())
+#while 1:
+    #data = conn.recv(BUFFER_SIZE)
+    #if not data: break
+    #print("received data:", data)
+
+#conn.close()
